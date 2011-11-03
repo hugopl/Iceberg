f3d9c74669986834fe7fc72850cafac2efc3f1cd
Anselmo L. S. Melo <anselmolsm@gmail.com>
2011-11-03 11:09:06 -0300

    Rename mainwindow file
    As the mainwindow does not depends on kdelibs anymore, there is no
    reasonable reason to keep the name mon-kde instead of mainwindow.
    
    Signed-off-by: Anselmo L. S. Melo <anselmolsm@gmail.com>

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -21,7 +21,7 @@
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include "mon-kde.h"
+#include "mainwindow.h"
 #include <QApplication>
 #include <QMenu>
 #include <QMenuBar>
@@ -254,4 +254,4 @@ int main(int argc, char** argv)
     return app.exec();
 }
 
-#include "mon-kde.moc"
+#include "mainwindow.moc"
