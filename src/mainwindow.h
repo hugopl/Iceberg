78e0090f2d04c6c9fa430d03805d20c2fe834399
Anselmo L. S. Melo <anselmolsm@gmail.com>
2012-01-06 00:56:48 -0300

    Systray optional
    Make the system tray optional. This commit also renames class members
    to use the prefix m_MEMBERNAME.
    
    Signed-off-by: Anselmo L. S. Melo <anselmolsm@gmail.com>

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,7 +5,7 @@
     Copyright (c) 2003,2004 Stephan Kulow <coolo@kde.org>
     Copyright (c) 2003,2004 Cornelius Schumacher <schumacher@kde.org>
     Copyright (c) 2011 Hugo Parente Lima <hugo.pl@gmail.com>
-    Copyright (c) 2011 Anselmo L. S. Melo <anselmolsm@gmail.com>
+    Copyright (c) 2011, 2012 Anselmo L. S. Melo <anselmolsm@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -80,8 +80,9 @@ private:
     QAction* m_starView;
     QAction* m_detailedView;
     QAction* m_configView;
-    QSystemTrayIcon* systemTrayIcon;
-    QMenu* systemTrayMenu;
+    QAction* m_systrayAction;
+    QSystemTrayIcon* m_systemTrayIcon;
+    QMenu* m_systemTrayMenu;
 };
 
 #endif // MAINWINDOW_H
