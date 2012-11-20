7c836226f4ea09397d491e129481db1a5a8fb715
Hugo Parente Lima <hugo.pl@gmail.com>
2012-11-20 00:44:31 -0300

    Fix meique.lua to work with a newer version.

--- meique.lua
+++ meique.lua
@@ -3,4 +3,4 @@ qtGui = findPackage("QtGui", "4.7", REQUIRED)
 qtNetwork = findPackage("QtNetwork", "4.7", REQUIRED)
 icecc = findPackage("icecc", "0.9.7", REQUIRED)
 
-addSubDirectory("src")
+addSubdirectory("src")
