d847e6e5c1021903850a0414c8089f5bd1071328
Hugo Parente Lima <hugo.lima@openbossa.org>
2014-04-11 16:03:53 -0300

    Fix compilation with Qt5 using meique and set it as default.

--- meique.lua
+++ meique.lua
@@ -1,15 +1,5 @@
-useQt5 = option("Qt5", "Use Qt5 instead of Qt4")
-qtVersion = "4.8"
-qtPrefix = "Qt"
-if useQt5 then
-    qtVersion = "5.0"
-    qtPrefix = "Qt5"
-end
+requiresMeique("0.94")
 
-qtCore = findPackage(qtPrefix.."Core", qtVersion, REQUIRED)
-qtGui = findPackage(qtPrefix.."Gui", qtVersion, REQUIRED)
-qtWidgets = useQt5:findPackage("Qt5Widgets", qtVersion, REQUIRED)
-qtNetwork = findPackage(qtPrefix.."Network", qtVersion, REQUIRED)
-icecc = findPackage("icecc", "0.9.7", REQUIRED)
+useQt5 = option("Qt5", "Use Qt5 instead of Qt4", "true")
 
 addSubdirectory("src")
--- src/meique.lua
+++ src/meique.lua
@@ -1,27 +1,47 @@
 iceberg = Executable:new("iceberg")
-iceberg:addFiles([[
-job.cpp
-main.cpp
-monitor.cpp
-mainwindow.cpp
-hostinfo.cpp
-statusview.cpp
-detailedhostview.cpp
-hostlistview.cpp
-joblistview.cpp
-listview.cpp
-starview.cpp
-]])
 
-iceberg:addQtResource("icons.qrc")
+if useQt5 then
+    qt = "Qt5"
+    qtWidgets = findPackage("Qt5Widgets")
+    iceberg:use(qtWidgets)
+else
+    qt = "Qt"
+end
+
+openGL = findPackage("gl")
+qtCore = findPackage(qt.."Core")
+qtGui = findPackage(qt.."Gui")
+qtGL = findPackage(qt.."OpenGL")
+qtNetwork = findPackage(qt.."Network")
+icecc = findPackage("icecc")
+freetype = findPackage("freetype2", REQUIRED)
+
 
 iceberg:use(qtCore)
 iceberg:use(qtGui)
+iceberg:use(openGL)
+iceberg:use(qtGL)
 iceberg:use(qtNetwork)
-useQt5:iceberg:use(qtWidgets)
 iceberg:use(icecc)
+iceberg:use(freetype)
+iceberg:addCustomFlags("-fPIC")
+
+iceberg:addFiles([[
+    job.cpp
+    main.cpp
+    monitor.cpp
+    mainwindow.cpp
+    hostinfo.cpp
+    statusview.cpp
+    detailedhostview.cpp
+    hostlistview.cpp
+    joblistview.cpp
+    listview.cpp
+    starview.cpp
+]])
+iceberg:addQtResource("icons.qrc")
 iceberg:useQtAutomoc()
 -- icecc uses dlopen but it's pkg-config doesn't tell this.
-UNIX:iceberg:addLinkLibraries("dl")
+iceberg:addLinkLibraries("dl")
 
 iceberg:install("bin")
